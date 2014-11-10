ef7e06f5966b5680a6c7059a5808eb0203821ea7
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-09 23:54:24 -0500

    main and interface methods

--- FAA_OCR/src/faa_ocr/Controller/AirportController.java
+++ FAA_OCR/src/faa_ocr/Controller/AirportController.java
@@ -1,3 +1,6 @@
+import faa_ocr.Controller.ArguementParser;
+
+
 
 /**
  * Class to control all airport behaviors
@@ -9,6 +12,38 @@ public class AirportController
 	public static void main(String[] args)
 	{
 		//Accept list of list of PDFS
+		for(String arg : args){
+			if(ArguementParser.parseArguement(arg))
+			{
+				//do everything to pdf document
+			}
+			else
+			{
+				//TODO: do we want to do anything if the path is not a pdf??
+			}
+		}
+	}
+	
+	
+	private String writeKML(File xml_file)
+	{
 		
 	}
+	
+	private String writeXML(Airport airport)
+	{
+		
+	}
+	
+	
+	private void parseVisualData(Airport airport)
+	{
+		
+	}
+	
+	private void parseTextData(Airport airport)
+	{
+		
+	}
+	
 }
