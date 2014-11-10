c1a941c804dba72e630995e0d709029748f7c594
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-09 23:53:34 -0500

    parseArguement() method

--- FAA_OCR/src/faa_ocr/Controller/ArguementParser.java
+++ FAA_OCR/src/faa_ocr/Controller/ArguementParser.java
@@ -1,28 +1,24 @@
-import java.nio.file.Files;
+package faa_ocr.Controller;
 
+import java.io.File;
+import java.nio.file.Files;
 
 
 public class ArguementParser 
 {
-	
-	public ArguementParser()
-	{
-		//do nothing
-	}
-	
 	
 	/**
 	 * Verify any paths that are inputted and make sure they are .pdf
 	 * @param path	path to file you want to verify
 	 * @return true if path is valid and it is a .pdf, false otherwise
 	 */
-	public boolean verifyPDFPaths(String[] paths)
+	public static boolean verifyPDFPaths(String[] paths)
 	{
 		
 		for (int i = 0; i < paths.length; i++)
 		{
 			File file = new File(paths[i]);
-			if(file.exisits())
+			if(file.exists())
 			{
 				if(Files.probeContentType(file.toPath()).equals("pdf"))
 				{
@@ -33,4 +29,27 @@ public class ArguementParser
 			
 		}
 	}
+	
+	/**
+	 * Verify an inputted path and make sure it is a .pdf
+	 * @param path	path to file you want to verify
+	 * @return true if path is valid and it is a .pdf, false otherwise
+	 */
+	public static boolean parseArguement(String path)
+	{
+			File file = new File(path);
+			if(file.exists())
+			{
+				if(Files.probeContentType(file.toPath()).equals("pdf"))
+				{
+					//TODO: How do we want to handle good/bad pdf files?
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+	}
+	
 }
