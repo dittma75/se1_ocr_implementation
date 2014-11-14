8645872483d8d58c54eaabae9342ddae5362ffcc
Joe Kvedaras <jokvedaras@gmail.com>
2014-11-14 11:50:22 -0500

    Arguement parser looks at first couple characters of pdf to make sure it equals %PDF. If it has those character, that file is a PDF

--- FAA_OCR/src/faa_ocr/Controller/AirportController.java
+++ FAA_OCR/src/faa_ocr/Controller/AirportController.java
@@ -1,49 +1,100 @@
-import faa_ocr.Controller.ArguementParser;
+package faa_ocr.Controller;
 
+import java.io.File;
+
+import faa_ocr.ADTs.Airport;
+import faa_ocr.Controller.ArguementParser;
 
 
 /**
  * Class to control all airport behaviors
- * @author jokvedaras
+ * @author Joe Kvedaras
  *
  */
 public class AirportController 
-{
+{	
+	
 	public static void main(String[] args)
-	{
+	{		
+		AirportController airport_controller = new AirportController();
+		
 		//Accept list of list of PDFS
 		for(String arg : args){
 			if(ArguementParser.parseArguement(arg))
 			{
-				//do everything to pdf document
+				//are we going to pass airports around 1 at a time as we see that the path is valid?
+				// or are we going to check everything, then call all the airports?
+				airport_controller.getInformationFromPDF(arg);
 			}
 			else
 			{
 				//TODO: do we want to do anything if the path is not a pdf??
+				System.out.println(arg + " is not a valid PDF");
 			}
 		}
+		System.out.println(ArguementParser.parseArguement("./res/ACY/00669AD.PDF"));
 	}
 	
 	
-	private String writeKML(File xml_file)
+	public AirportController()
 	{
+		//initialize AirportController object
+		//TODO: may choose to hold instances of all the objects so we don't have to create them again for every pdf file.
+	}
+	
+	//only classes in same package can call getInformationFromPDF
+	private void getInformationFromPDF(String path)
+	{
+		//create a new airport with Sting path to PDF
+		Airport airport = new Airport(path);
+		
+		//get textual data from PDF
+		parseTextData(airport);
+		
+		//get visual data from PDF
+		parseVisualData(airport);
+		
+		//turn Airport into an XML and save path to XML
+		String path_to_xml = XXMLParser.writeXML(airport);
+		
+		//turn xml file into a kml file.
+		//TODO: Strings or Files from xml???
+		String path_to_kml = KMLParser.writeKML(new File(path_to_xml));
+		
+		//print out results of transformations
+		printResults(airport, path_to_xml,path_to_xml);
 		
 	}
 	
-	private String writeXML(Airport airport)
+	private void printResults(Airport airport, String path_to_xml, String path_to_kml)
 	{
-		
+		System.out.println();
+		System.out.println(airport.name)
+		System.out.println("Path to XML: " + path_to_xml);
+		System.out.println("Path to KML: " + path_to_kml);
+		System.out.println();
 	}
 	
 	
-	private void parseVisualData(Airport airport)
-	{
-		
-	}
-	
-	private void parseTextData(Airport airport)
-	{
-		
-	}
+//	private String writeKML(File xml_file)
+//	{
+//		return KMLParser.writeKML(xml_file);
+//	}
+//	
+//	private String writeXML(Airport airport)
+//	{
+//		return XMLParser.writeXML(airport);
+//	}
+//	
+//	
+//	private void parseVisualData(Airport airport)
+//	{
+//		
+//	}
+//	
+//	private void parseTextData(Airport airport)
+//	{
+//		
+//	}
 	
 }
--- FAA_OCR/src/faa_ocr/Controller/ArguementParser.java
+++ FAA_OCR/src/faa_ocr/Controller/ArguementParser.java
@@ -1,54 +1,83 @@
 package faa_ocr.Controller;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.nio.file.Files;
+import java.io.FileReader;
+import java.io.IOException;
 
-
+/**
+ * 
+ * @author Joe Kvedaras
+ *
+ */
 public class ArguementParser 
 {
 	
-	/**
-	 * Verify any paths that are inputted and make sure they are .pdf
-	 * @param path	path to file you want to verify
-	 * @return true if path is valid and it is a .pdf, false otherwise
-	 */
-	public static boolean verifyPDFPaths(String[] paths)
-	{
-		
-		for (int i = 0; i < paths.length; i++)
-		{
-			File file = new File(paths[i]);
-			if(file.exists())
-			{
-				if(Files.probeContentType(file.toPath()).equals("pdf"))
-				{
-					//TODO: How do we want to handle good/bad pdf files?
-				}
-			}
-			
-			
-		}
-	}
+//	/**
+//	 * Verify any paths that are inputted and make sure they are .pdf
+//	 * @param path	path to file you want to verify
+//	 * @return true if path is valid and it is a .pdf, false otherwise
+//	 */
+//	public static boolean verifyPDFPaths(String[] paths)
+//	{
+//		
+//		for (int i = 0; i < paths.length; i++)
+//		{
+//			File file = new File(paths[i]);
+//			if(file.exists())
+//			{
+//				if(Files.probeContentType(file.toPath()).equals("pdf"))
+//				{
+//					//TODO: How do we want to handle good/bad pdf files?
+//				}
+//			}
+//			
+//			
+//		}
+//	}
 	
 	/**
-	 * Verify an inputted path and make sure it is a .pdf
+	 * Verify an inputed path and make sure it is a .pdf
 	 * @param path	path to file you want to verify
 	 * @return true if path is valid and it is a .pdf, false otherwise
+	 * @throws IOException 
 	 */
-	public static boolean parseArguement(String path)
+	public static boolean parseArguement(String pdf_file_path)
 	{
-			File file = new File(path);
+			File file = new File(pdf_file_path);
 			if(file.exists())
 			{
-				if(Files.probeContentType(file.toPath()).equals("pdf"))
+				//Open up the file and check the first 4 characters to make sure it equals %PDF
+				try
 				{
-					//TODO: How do we want to handle good/bad pdf files?
-					return true;
+					BufferedReader reader = new BufferedReader(new FileReader(file));
+					String line = reader.readLine();
+					System.out.println(line);
+					
+					if(line.startsWith("%PDF"))
+					{
+						reader.close();
+						return true;
+					}
+					else
+					{
+						reader.close();
+						return false;
+					}
+					
 				}
-				else
+				catch (IOException e)
 				{
-					return false;
+					System.err.println("IO Exception when checking if path is a PDF");
+					e.printStackTrace();
 				}
+				
+				return false;
+				
+			}
+			else{
+				//file does not exist.
+				return false;
 			}
 	}
 	
